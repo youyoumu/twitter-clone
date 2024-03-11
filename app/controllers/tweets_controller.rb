38de717ce656b73c9ea74963700d472dce3e9799
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-03-11 14:50:57 +0700

    User can tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,25 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all
     @tweet = Tweet.new
   end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+
+    if @tweet.save
+      redirect_to tweet_path(@tweet)
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  belongs_to :parent
+  belongs_to :parent, class_name: 'Tweet', optional: true
+  has_many :replies, class_name: 'Tweet', foreign_key: 'parent_id'
+
+  validates :content, presence: true, length: { minimum: 1 }
 end
