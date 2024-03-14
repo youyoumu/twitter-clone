57f22ee18f4f21cf665f5595eaf83c738cb3eedd
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-03-14 15:18:39 +0700

    Update assosiation name

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -3,13 +3,13 @@ class FollowsController < ApplicationController
     return if current_user == User.find(params[:user_id])
 
     begin
-      current_user.followings << User.find(params[:user_id])
+      current_user.following << User.find(params[:user_id])
     rescue ActiveRecord::RecordNotUnique
       # do nothing
     end
   end
 
   def destroy
-    current_user.followings.delete(User.find(params[:user_id]))
+    current_user.following.delete(User.find(params[:user_id]))
   end
 end
