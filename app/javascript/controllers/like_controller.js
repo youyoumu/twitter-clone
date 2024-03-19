1b11872dd73199a2bf4cd6708fd6b2eaf8e4dcb2
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-03-20 00:12:18 +0700

    Increase/decrease like count

--- app/javascript/controllers/like_controller.js
+++ app/javascript/controllers/like_controller.js
@@ -1,14 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["unlikeButton", "likeButton"]
+    static targets = ["unlikeButton", "likeButton", "likeCount"]
+
+    connect() {
+        console.log(this.likeCountTarget.textContent)
+    }
 
     like() {
+        this.likeCountTarget.textContent = parseInt(this.likeCountTarget.textContent) + 1
         this.unlikeButtonTarget.hidden = false
         this.likeButtonTarget.hidden = true
     }
 
     unlike() {
+        this.likeCountTarget.textContent = parseInt(this.likeCountTarget.textContent) - 1
         this.unlikeButtonTarget.hidden = true
         this.likeButtonTarget.hidden = false
     }
